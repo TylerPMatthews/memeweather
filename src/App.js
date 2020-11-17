3a3635cf494d9b92df8b56b24b92e1d1d4454e32
ty <tylerpmatthews1998@gmail.com>
2020-11-17 13:47:22 -0500

    chnaged background and chnaged font in App

--- src/App.js
+++ src/App.js
@@ -1,49 +1,58 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Weather from "./components/Weather";
 import axios from "axios";
-import styled from 'styled-components';
+import styled from "styled-components";
 const StyledApp = styled.div`
+  h2 {
+    color: red;
+    text-align: center;
+    margin-top: 5%;
+    font-family: 'Architects Daughter', cursive;
+  }
+  .search-box {
+    display: flex;
+    justify-content: center;
+  }
+  input {
+    text-align: center;
+    box-sizing: border-box;
+    padding: 0% 5%;
+    border: 1px solid black;
+    font-size: 1.3rem;
+  }
+  .no-weather {
+    text-align: center;
+    margin-top: 15%;
+    font-size: 1.5rem;
+    color: red;
+    margin-right: 5%;
+    margin-left: 5%;
+    font-family: 'Architects Daughter', cursive;
+  }
+  .code {
+    display: flex;
+    justify-content: center;
+  }
+  button {
+    font-size: 1.2rem;
+    border-radius: 12px;
+    margin-bottom: 5%;
+    color: red;
+    background-color: black;
+    border-color: red;
+    font-family: 'Architects Daughter', cursive;
+  }
+  button:hover {
+    color: white;
+    border-color:white;
 
-h2{
-  color:red;
-  text-align:center;
-  margin-top:5%;
-}
-.search-box{
-  display:flex;
-  justify-content:center;
-}
-input{
-  text-align:center;
-  box-sizing: border-box;
-  padding:0% 5%;
-  border:1px solid black;
-  font-size:1.3rem;
-
-  
-}
-.no-weather{
-  text-align:center;
-  margin-top:15%;
-  font-size:1.5rem;
-  color:red;
-}
-.code{
-  display:flex;
-  justify-content:center;
-}
-button{
-  font-size:1.2rem;
-  border-radius:12px;
-  margin-bottom:5%;
-  color:red;
-  background-color:black;
-  border-color:red;
-}
-
-
-`
+  }
+  footer p{
+    text-align:center;
+    font-family: 'Architects Daughter', cursive;
+  }
+`;
 function App() {
   const [query, setQuery] = useState("");
   const [weather, setWeather] = useState();
@@ -65,7 +74,6 @@ function App() {
   };
 
   return (
-    
     <StyledApp>
       <h2>Meme Weather</h2>
       <div className="search-box">
@@ -77,26 +85,29 @@ function App() {
           value={query}
           onKeyPress={search}
         ></input>
-        </div>
-       
+      </div>
+
       {weather === undefined ? (
-        <div className='no-weather'>
-        <p>No weather to display</p>
-        <p>Press the Enter key to submit City,State</p>
+        <div className="no-weather">
+          <p>No weather to display</p>
+          <p>Press the Enter key to submit City,State</p>
         </div>
       ) : (
-      
         <Weather weather={weather} />
       )}
-      <div className='code'>
-      <button onClick={(evt)=>{
-        window.alert('Created by: https://github.com/TylerPMatthews')
-      }}>
-      Creator
-    </button>
-    </div>
+      <div className="code">
+        <button
+          onClick={(evt) => {
+            window.alert("Created by: https://github.com/TylerPMatthews");
+          }}
+        >
+          Creator
+        </button>
+      </div>
+      <footer>
+        <p>Meme Weather 2020</p>
+      </footer>
     </StyledApp>
-    
   );
 }
 
